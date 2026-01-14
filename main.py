b2950a5158bad63497f365d025d6af3c45595a7d
badriawan <yusufbadriawan@gmail.com>
2026-01-14 20:48:12 +0800

    nir to thermal

--- main.py
+++ main.py
@@ -190,20 +190,20 @@ fused_save = (fused_img * 255).astype("uint8")
 cv2.imwrite("fusion_result.png", fused_save)
 
 
-thermal = inputs[0,0].cpu().numpy()
-nir = inputs[0,1].cpu().numpy()
+nir = inputs[0,0].cpu().numpy()
+thermal = inputs[0,1].cpu().numpy()
 rgb = inputs[0,2].cpu().numpy()
 
 plt.figure(figsize=(12,3))
 
 plt.subplot(1,4,1)
-plt.imshow(thermal, cmap='gray')
-plt.title("thermal")
+plt.imshow(nir, cmap='gray')
+plt.title("nir")
 plt.axis("off")
 
 plt.subplot(1,4,2)
-plt.imshow(nir, cmap='gray')
-plt.title("nir")
+plt.imshow(thermal, cmap='hot')
+plt.title("thermal")
 plt.axis("off")
 
 plt.subplot(1,4,3)
@@ -223,12 +223,12 @@ plt.close()
 #Original_Images
 os.makedirs("original_images", exist_ok=True)
 
-thermal = inputs[0,0].cpu().numpy()
-nir = inputs[0,1].cpu().numpy()
+nir = inputs[0,0].cpu().numpy()
+thermal = inputs[0,1].cpu().numpy()
 rgb = inputs[0,2].cpu().numpy()
 
-cv2.imwrite("original_images/thermal.png", (thermal * 255).astype("uint8"))
 cv2.imwrite("original_images/nir.png", (nir * 255).astype("uint8"))
+cv2.imwrite("original_images/thermal.png", (thermal * 255).astype("uint8"))
 cv2.imwrite("original_images/rgb.png", (rgb * 255).astype("uint8"))
 
 
